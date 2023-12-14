635f28d260637b7a22bce8b9825fe40e8cdefcc7
Peter Riemersma <35268498+GroteGehaktBal@users.noreply.github.com>
2023-12-14 11:25:30 +0100

    feat: :sparkles: New feature to check for .git directory in project directories
    This commit adds a new feature to the script that checks whether the directories for opening projects contain a .git directory.

--- index.js
+++ index.js
@@ -1,53 +1,75 @@
-const { readdirSync } = require("fs");
+// Import necessary modules
+const { readdirSync, existsSync } = require("fs");
+const { join } = require("path");
 
+// Get environment variables
 const PROJECT_DIR = process.env.PROJECT_DIR;
 const INC_SUBDIRS = !!+process.env.INC_SUBDIRS;
+const WITH_GITFILE = !!+process.env.WITH_GITFILE; // New variable for checking .git files
 const action = process.argv[2];
 
+// Function to get directories in a given path
 const getDirectories = (source) =>
   readdirSync(source, { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
 
-const dirs = getDirectories(PROJECT_DIR);
+// Function to check if a directory contains a ".git" subdirectory
+const isGitRepo = (dir) => {
+  if (action === "open" && WITH_GITFILE) {
+    const gitPath = join(PROJECT_DIR, dir, ".git");
+    return existsSync(gitPath) && readdirSync(gitPath).length > 0;
+  } else {
+    return true; // Always include the directory for new projects or when WITH_GITFILE is not checked
+  }
+};
 
+// Function to create an item object
+const createItem = (title, subtitle, arg) => ({ title, subtitle, arg });
+
+// Array to store items
 const items = [];
+
+// Check the action and perform corresponding actions
 if (action === "new") {
+  // Create items for new projects
   if (INC_SUBDIRS) {
-    dirs.forEach((dir) => {
-      items.push({
-        title: dir,
-        subtitle: `Create project in directory: \`${dir}\``,
-        arg: `${PROJECT_DIR}/${dir}`,
-      });
+    getDirectories(PROJECT_DIR).forEach((dir) => {
+      if (isGitRepo(dir)) {
+        items.push(
+          createItem(dir, `Create project in directory: \`${dir}\``, `${PROJECT_DIR}/${dir}`)
+        );
+      }
     });
   } else {
-    items.push({
-      title: "New Project",
-      subtitle: `Create project in directory: \`${PROJECT_DIR}\``,
-      arg: `${PROJECT_DIR}`,
-    });
+    if (isGitRepo("")) {
+      items.push(
+        createItem("New Project", `Create project in directory: \`${PROJECT_DIR}\``, PROJECT_DIR)
+      );
+    }
   }
 } else if (action === "open") {
+  // Process projects for opening
+  const processDirs = (dir, project) => {
+    const fullPath = `${PROJECT_DIR}/${dir}/${project}`;
+    if (isGitRepo(`${dir}/${project}`)) {
+      items.push(
+        createItem(project, `Open project \`${project}\` from directory \`${dir}\``, fullPath)
+      );
+    }
+  };
+
+  // Check whether to include subdirectories
   if (INC_SUBDIRS) {
-    dirs.forEach((dir) => {
-      getDirectories(`${PROJECT_DIR}/${dir}`).forEach((project) => {
-        items.push({
-          title: project,
-          subtitle: `Open project \`${project}\` from directory \`${dir}\``,
-          arg: `${PROJECT_DIR}/${dir}/${project}`,
-        });
-      });
+    getDirectories(PROJECT_DIR).forEach((dir) => {
+      getDirectories(`${PROJECT_DIR}/${dir}`).forEach((project) =>
+        processDirs(dir, project)
+      );
     });
   } else {
-    dirs.forEach((project) => {
-      items.push({
-        title: project,
-        subtitle: `Open project \`${project}\``,
-        arg: `${PROJECT_DIR}/${project}`,
-      });
-    });
+    getDirectories(PROJECT_DIR).forEach((project) => processDirs("", project));
   }
 }
 
+// Output items as JSON
 console.log(JSON.stringify({ items }, null, "\t"));
